fa9fdcb3b0e84be7d785949e1869dfbb41444010
fabian-sp <fabian.schaipp@hotmail.de>
2020-01-17 15:34:12 +0100

    test run

--- exp_microbiome.py
+++ exp_microbiome.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 from microbiome_helper import load_and_transform
 from gglasso.solver.ext_admm_solver import ext_ADMM_MGL
+
 from gglasso.helper.experiment_helper import lambda_parametrizer
 from gglasso.helper.ext_admm_helper import get_K_identity, check_G, load_G, save_G
 
@@ -31,6 +32,11 @@ check_G(G, p)
 Omega_0 = get_K_identity(p)
 
 sol, info = ext_ADMM_MGL(S, lambda1, lambda2, 'GGL', Omega_0, G, eps_admm = 1e-3, verbose = True)
+
+
+#from gglasso.solver.ext_latent_admm_solver import ext_ADMM_MGL
+#mu1 = .1
+#sol, info = ext_ADMM_MGL(S, lambda1, lambda2, mu1, 'GGL', Omega_0, G, eps_admm = 1e-3, verbose = True)
 
 
 Theta = sol['Theta']
