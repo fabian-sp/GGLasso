78fa022c96b0d0a3dbcd7ed889e41fef0c0b6385
fabian-sp <fabian.schaipp@hotmail.de>
2020-10-01 15:53:42 +0200

    remove prints

--- gglasso/solver/fgl_helper.py
+++ gglasso/solver/fgl_helper.py
@@ -25,19 +25,16 @@ def condat_method(y,lam):
         
         while k == N-1:
             if umin < 0:
-                #print('c1')
                 x[k0:kminus +1] = vmin
                 kminus += 1
                 k = kplus = k0 = kminus
                 umin = lam; vmin = y[k]; umax = y[k] + lam - vmax 
             elif umax > 0:
-                #print('c2')
                 x[k0:kplus+1] = vmax
                 kplus += 1
                 k  = kminus = k0 = kplus
                 umax = -lam; vmax = y[k]; umin = y[k] - lam - vmin
             else:
-                #print('c3')
                 x[k0:] = vmin + umin/(k-k0+1)
                 return x
             
@@ -46,15 +43,13 @@ def condat_method(y,lam):
                 return x
     
         if y[k+1] + umin - vmin < -lam:
-            #print('b1')
             x[k0:kminus+1] = vmin
             kminus += 1
             k = kplus = k0 = kminus
             vmin = y[k]; vmax = y[k] + 2*lam
             umin = lam; umax = -lam
         
-        elif y[k+1] + umax -vmax > lam:
-            #print('b2')           
+        elif y[k+1] + umax -vmax > lam:         
             x[k0:kplus+1] = vmax
             kplus += 1
             k  = kminus = k0 = kplus
@@ -62,24 +57,23 @@ def condat_method(y,lam):
             umin = lam; umax = -lam
                      
         else:
-            #print('b3')
             k += 1
             umin = umin + y[k] - vmin
             umax = umax + y[k] - vmax
             if umin >= lam:
-                #print('b31')
                 vmin += (umin-lam)/(k-k0+1); umin = lam; kminus = k
             if umax <= -lam:
-                #print('b32')
                 vmax += (umax+lam)/(k-k0+1); umax = -lam; kplus = k
          
     return 
 
+# for testing
 
-def objective(x, y, l1):
-    res = l1* abs(x[1:] - x[0:-1]).sum() + .5*np.linalg.norm(x-y)**2
+#def objective(x, y, l1):
+#    res = l1* abs(x[1:] - x[0:-1]).sum() + .5*np.linalg.norm(x-y)**2
     
-    return res
+#    return res
+
 
 #l1 = .1
 #
