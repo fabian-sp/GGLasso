15a1c0fd940d7cd7deeebc61b70887208513c674
fabian-sp <fabian.schaipp@hotmail.de>
2020-11-24 10:26:25 +0100

    remove typo in condats algo, has no impact

--- gglasso/solver/fgl_helper.py
+++ gglasso/solver/fgl_helper.py
@@ -27,12 +27,12 @@ def condat_method(y,lam):
             if umin < 0:
                 x[k0:kminus +1] = vmin
                 kminus += 1
-                k = kplus = k0 = kminus
+                k = k0 = kminus
                 umin = lam; vmin = y[k]; umax = y[k] + lam - vmax 
             elif umax > 0:
                 x[k0:kplus+1] = vmax
                 kplus += 1
-                k  = kminus = k0 = kplus
+                k  = k0 = kplus
                 umax = -lam; vmax = y[k]; umin = y[k] - lam - vmin
             else:
                 x[k0:] = vmin + umin/(k-k0+1)
@@ -69,22 +69,23 @@ def condat_method(y,lam):
 
 # for testing
 
-#def objective(x, y, l1):
-#    res = l1* abs(x[1:] - x[0:-1]).sum() + .5*np.linalg.norm(x-y)**2
+# def objective(x, y, l1):
+#     res = l1* abs(x[1:] - x[0:-1]).sum() + .5*np.linalg.norm(x-y)**2
     
-#    return res
+#     return res
 
 
 #l1 = .1
 #
-#for i in range(1000):
-#    print(i)
-#    y = np.random.rand(100)
-#    x = condat_method(y, l1)
-#    x2 = ProxTV(l1).call(y)
-#    
-#    print(np.linalg.norm(x-x2))
-#    print("Condat function correct: ", objective(x, y, l1) <= objective(x2, y, l1) + 1e-5)
+# for i in range(1000):
+#     print(i)
+#     y = np.random.rand(100)
+#     x = condat_method(y, l1)
+#     x2 = ProxTV(l1).call(y)
+    
+#     print(np.linalg.norm(x-x2))
+#     print("Condat function correct: ", objective(x, y, l1) <= objective(x2, y, l1) + 1e-5)
+#     assert  np.linalg.norm(x-x2) <= 1e-10
     
 
 
