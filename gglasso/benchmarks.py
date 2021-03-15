3d128674b3b8391fb1d93682d71d11fddf0677b1
Oleg <otorrent@mail.ru>
2021-03-15 12:52:31 +0100

    wrapped benchmarks into functions

--- gglasso/benchmarks.py
+++ gglasso/benchmarks.py
@@ -16,52 +16,7 @@ from gglasso.helper.model_selection import single_grid_search
 
 from regain.covariance import GraphicalLasso as rg_GL
 
-# def sampling(N, p, K, M):
-#     Sigma, Theta = group_power_network(p, K=K, M=M)  # Theta is true precision matrix
-#     S, samples = sample_covariance_matrix(Sigma, N)
-#
-#     S = S[0, :, :]
-#     Theta = Theta[0, :, :]  # true precision matrix
-#     return S, Theta
-
-p = 100
-N = 200
-Sigma, Theta = group_power_network(p, K=5, M=2)  # Theta is true precision matrix
-
-S, samples = sample_covariance_matrix(Sigma, N)
-
-S = S[0, :, :]
-Theta = Theta[0, :, :]  # true precision matrix
-
-lambda1 = 0.01
-Omega_0 = np.eye(p)
-
-model_1 = rg_GL(alpha=0.01, max_iter=100, tol=1e-4).fit(samples[0, :, :].T)
-model_2 = sk_GL(alpha=0.01, max_iter=100, tol=0.01).fit(samples[0, :, :].T)
-sol, info = ADMM_SGL(S, lambda1, Omega_0, max_iter=100, tol=1e-4, rtol=1e-4,
-                     stopping_criterion="boyd",
-                     verbose=False, latent=False)
-type(model_2.precision_)
-
-model = sk_GL()
-model(alpha=0.01).fit(samples[0, :, :].T)
-
-dict_models = {"sklearn": sk_GL(), "regain": rg_GL()}
-for model, model_instant in dict_models.items():
-    print(model, model_instant)
-
-dict_models = dict()
-for tol in [0.01, 0.02]:
-    for rtol in [0.01, 0.02]:
-        dict_models['regain_' + "tol_" + str(tol) + "_rtol_" + str(rtol)] = rg_GL(alpha=lambda1, max_iter=100, tol=tol,
-                                                                                  rtol=rtol)
-    for enet_tol in [1, 0.01]:
-        dict_models['sklearn' + "tol_" + str(tol) + "_enet_tol_" + str(rtol)] = sk_GL(alpha=lambda1, max_iter=100,
-                                                                                      tol=tol,
-                                                                                      enet_tol=enet_tol)
-
-
-def models_to_dict(models=None, tol_list=list, rtol_list=list, enet_list=list, max_iter=int):
+def models_to_dict(models=None, lambda1=0.1, tol_list=list, rtol_list=list, enet_list=list, max_iter=50000):
     if models is None:
         models = list(str)
     models_dict = dict()
@@ -71,47 +26,64 @@ def models_to_dict(models=None, tol_list=list, rtol_list=list, enet_list=list, m
 
             if model == "regain":
                 models_dict[str(model) + "_tol_" + str(tol) + "_rtol_" + str(rtol)] \
-                    = rg_GL(alpha=lambda1, max_iter=max_iter, tol=tol, rtol=rtol)
+                    = rg_GL(alpha=lambda1, tol=tol, rtol=rtol, max_iter=max_iter)
 
             elif model == "sklearn":
-                models_dict[str(model) + "_tol_" + str(tol) + "_enet_tol_" + str(enet_tol)] \
-                    = sk_GL(alpha=lambda1, max_iter=max_iter, tol=tol, enet_tol=enet_tol)
-
-            # if model == "admm":
+                models_dict[str(model) + "_tol_" + str(tol) + "_enet_" + str(enet_tol)] \
+                    = sk_GL(alpha=lambda1, tol=tol, enet_tol=enet_tol, max_iter=max_iter)
 
     return models_dict
 
 
-def time_benchmark(models=dict, Z=np.array([]), cov_dict=dict(), precision_dict=dict(),
+def sklearn_time_benchmark(models=dict, X=np.array([]), Z=np.array([]),
+                   cov_dict=dict(), precision_dict=dict(),
                    time_dict=dict(), accuracy_dict=dict()):
-
     for model, model_instant in models.items():
         start = time.time()
-        Z_n = model_instant.fit(samples[0, :, :].T)
+        Z_i = model_instant.fit(X)
         end = time.time()
 
         hours, rem = divmod(end - start, 3600)
         minutes, seconds = divmod(rem, 60)
         time_dict[model] = "{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds)
 
-        cov_dict[model] = Z_n.covariance_
-        precision_dict[model] = Z_n.precision_
+        cov_dict["cov_" + model] = Z_i.covariance_
+        precision_dict["precision_" + model] = Z_i.precision_
 
-    for model, Z_n in precision_dict.items():
-        accuracy = np.linalg.norm(Z - np.array(Z_n)) / np.linalg.norm(Z)
+    for model, Z_i in precision_dict.items():
+        accuracy = np.linalg.norm(Z - np.array(Z_i)) / np.linalg.norm(Z)
         accuracy_dict[model] = accuracy
 
-    return accuracy_dict
+    return time_dict, accuracy_dict
 
 
-b = time_benchmark(models=a, Z=model_1.precision_)
+def admm_time_benchmark(S=np.array([]), Omega_0=np.array([]), Z=np.array([]), lambda1=float, max_iter=50000,
+                        method_list=list, stop_list=list, tol_list=list, rtol_list=list,
+                        cov_dict=dict(), precision_dict=dict(),
+                        time_dict=dict(), accuracy_dict=dict()):
+    for method in method_list:
+        for tol, rtol, stop in itertools.product(tol_list, rtol_list, stop_list):
+            if method == "single":
+                start = time.time()
+                Z_i, info = ADMM_SGL(S, lambda1=lambda1, Omega_0=Omega_0, max_iter=max_iter,
+                                     tol=tol, rtol=rtol, stopping_criterion=stop)
+                end = time.time()
 
-len(b)
+            elif method == "block":
+                start = time.time()
+                Z_i = block_SGL(S, lambda1=lambda1, Omega_0=Omega_0, max_iter=max_iter,
+                                tol=tol, rtol=rtol, stopping_criterion=stop)
+                end = time.time()
 
-str_tol = [str(x) for x in tol_list]
-method_list = ["sklearn"] * len(accuracy_list)
-df_sk = pd.DataFrame(data={'time': time_list,
-                           'distance': accuracy_list,
-                           'method': method_list,
-                           'tol_rate': tol_list,
-                           'str_tol': str_tol})
+            hours, rem = divmod(end - start, 3600)
+            minutes, seconds = divmod(rem, 60)
+            key = method + "-" + str(stop) + "_tol_" + str(tol) + "_rtol_" + str(rtol)
+            time_dict[key] = "{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds)
+
+            cov_dict["cov_" + key] = Z_i["Omega"]
+            precision_dict["precision_" + key] = Z_i["Theta"]
+
+            accuracy = np.linalg.norm(Z - np.array(Z_i["Theta"])) / np.linalg.norm(Z)
+            accuracy_dict["accuracy_" + key] = accuracy
+
+    return time_dict, accuracy_dict
