a8db218254b2ff19300b4196a2531ab631c44f29
Vlasovets <otorrent@mail.ru>
2021-03-18 17:16:59 +0100

    fixed the bug in sklearn_benchmark model initialization added sparsity benchmark based on gini index

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -8,6 +8,8 @@ from tqdm import trange
 
 from sklearn.covariance import GraphicalLasso as sk_GL
 from sklearn.covariance import empirical_covariance
+from sklearn import set_config
+set_config(print_changed_only=False)
 
 # sys.path.append('..')
 from gglasso.solver.single_admm_solver import ADMM_SGL
@@ -18,7 +20,7 @@ from gglasso.helper.model_selection import single_grid_search
 from regain.covariance import GraphicalLasso as rg_GL
 
 
-def models_to_dict(models=None, lambda1=0.1, tol_list=list, rtol_list=list, enet_list=list, max_iter=50000):
+def models_to_dict(models=None, lambda1=float, tol_list=list, rtol_list=list, enet_list=list, max_iter=50000):
     if models is None:
         models = list(str)
     models_dict = dict()
@@ -28,11 +30,13 @@ def models_to_dict(models=None, lambda1=0.1, tol_list=list, rtol_list=list, enet
 
             if model == "regain":
                 models_dict[str(model) + "_tol_" + str(tol) + "_rtol_" + str(rtol)] \
-                    = rg_GL(alpha=lambda1, tol=tol, rtol=rtol, max_iter=max_iter, assume_centered=True)
+                    = rg_GL(alpha=lambda1, tol=tol, rtol=rtol, max_iter=max_iter,
+                            assume_centered=True)
 
             elif model == "sklearn":
                 models_dict[str(model) + "_tol_" + str(tol) + "_enet_" + str(enet_tol)] \
-                    = sk_GL(alpha=lambda1, tol=tol, enet_tol=enet_tol, max_iter=max_iter, assume_centered=True)
+                    = sk_GL(alpha=lambda1, tol=tol, enet_tol=enet_tol, max_iter=max_iter,
+                            assume_centered=True)
 
     return models_dict
 
@@ -59,14 +63,13 @@ def sklearn_time_benchmark(models=dict, X=np.array([]), Z=np.array([]), n_iter=1
         accuracy = np.linalg.norm(Z - np.array(Z_i)) / np.linalg.norm(Z)
         accuracy_dict[model] = accuracy
 
-    return time_dict, accuracy_dict
+    return time_dict, accuracy_dict, precision_dict
 
 
 def admm_time_benchmark(S=np.array([]), Omega_0=np.array([]), Z=np.array([]), lambda1=float, n_iter=10, max_iter=50000,
                         method_list=list, stop_list=list, tol_list=list, rtol_list=list,
                         cov_dict=dict(), precision_dict=dict(),
                         time_dict=dict(), accuracy_dict=dict()):
-
     for method in method_list:
         for tol, rtol, stop in itertools.product(tol_list, rtol_list, stop_list):
 
@@ -97,22 +100,47 @@ def admm_time_benchmark(S=np.array([]), Omega_0=np.array([]), Z=np.array([]), la
             accuracy = np.linalg.norm(Z - np.array(Z_i["Theta"])) / np.linalg.norm(Z)
             accuracy_dict["accuracy_" + key] = accuracy
 
-    return time_dict, accuracy_dict
+    return time_dict, accuracy_dict, precision_dict
 
 
 def model_solution(model=str, X=np.array([]), lambda1=float, max_iter=50000, tol=float, rtol=float, enet=float):
+
     if model == "sklearn":
+
         start = time.time()
-        Z = sk_GL(alpha=lambda1, max_iter=max_iter, tol=tol, enet_tol=enet).fit(X)
+        Z = sk_GL(alpha=lambda1, max_iter=max_iter, tol=tol, enet_tol=enet, assume_centered=True).fit(X)
+        info = sk_GL(alpha=lambda1, max_iter=max_iter, tol=tol, enet_tol=enet, assume_centered=True)
         end = time.time()
 
     elif model == "regain":
+
         start = time.time()
-        Z = sk_GL(alpha=lambda1, max_iter=max_iter, tol=tol, rtol=rtol).fit(X)
+        Z = rg_GL(alpha=lambda1, max_iter=max_iter, tol=tol, rtol=rtol, assume_centered=True).fit(X)
+        info = rg_GL(alpha=lambda1, max_iter=max_iter, tol=tol, enet_tol=enet, assume_centered=True)
         end = time.time()
 
     hours, rem = divmod(end - start, 3600)
     minutes, seconds = divmod(rem, 60)
     Z_time = "{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds)
 
+    print(model, info)
+
     return Z.precision_, Z_time
+
+
+def gini(array):
+    """Calculate the Gini coefficient of a numpy array."""
+
+    # based on bottom eq: http://www.statsdirect.com/help/content/image/stat0206_wmf.gif
+    # from: http://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
+    array = array.flatten()  # all values are treated equally, arrays must be 1d
+    if np.amin(array) < 0:
+        array -= np.amin(array)  # values cannot be negative
+    array += 1e-20  # values cannot be 0
+    array = np.sort(array)  # values must be sorted
+    index = np.arange(1, array.shape[0] + 1)  # index per array element
+    n = array.shape[0]  # number of array elements
+
+    gini_coef = (np.sum((2 * index - n - 1) * array)) / (n * np.sum(array))  # Gini coefficient
+
+    return gini_coef
