528ef9f41d0d17e2d6f52c8797550281a5ae3687
fabian-sp <fabian.schaipp@hotmail.de>
2021-06-24 16:51:29 +0200

    change basic example

--- examples/plot_basic_example.py
+++ examples/plot_basic_example.py
@@ -3,8 +3,8 @@ Basic example
 =================================
 
 We demonstrate how to use ``GGLasso`` for a SGL problem. 
-First, we generate a sparse powerlaw network of 20 nodes. We generate an according precision matrix and sample from it. Here, we use a very large 
-number of samples (N=5000) to demonstrate that it is possible to recover (approximately) the original graph if sufficiently many samples are available.
+First, we generate a sparse Erdos-Renyi network of 20 nodes. We generate an according precision matrix and sample from it. Here, we use a large 
+number of samples (N=1000) to demonstrate that it is possible to recover (approximately) the original graph if sufficiently many samples are available.
 
 In many practical applications however, we face the situation of p>N.
 
@@ -21,10 +21,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 p = 20
-N = 5000
+N = 1000
 
-Sigma, Theta = group_power_network(p, K = 1, M = 1, nxseed = 1235)
-Sigma, Theta = generate_precision_matrix(p=p, M=1, style = 'powerlaw', gamma = 2.8, nxseed = 12345)
+Sigma, Theta = generate_precision_matrix(p=p, M=1, style='erdos', prob=0.1, nxseed=1234)
 
 S, sample = sample_covariance_matrix(Sigma, N)
 
@@ -40,7 +39,7 @@ A = adjacency_matrix(Theta)
 np.fill_diagonal(A,1)
 
 G = nx.from_numpy_array(A)
-pos = nx.drawing.layout.spring_layout(G)
+pos = nx.drawing.layout.spring_layout(G, seed = 1234)
 
 plt.figure()
 nx.draw_networkx(G, pos = pos, node_color = "darkblue", edge_color = "darkblue", font_color = 'white', with_labels = True)
@@ -51,17 +50,17 @@ nx.draw_networkx(G, pos = pos, node_color = "darkblue", edge_color = "darkblue",
 # We now create an instance of ``glasso_problem``. The problem formulation is derived automatically from the input shape of ``S``.
 #
 
-P = glasso_problem(S, N, reg_params = {'lambda1': 0.05}, latent = False, do_scaling = False)
+P = glasso_problem(S, N, reg_params = {'lambda1': 0.05}, latent = False, do_scaling = True)
 print(P)
 
 #%%
 # Next, do model selection by solving the problem on a range of :math:`\lambda_1` values.
 #
 
-lambda1_range = np.logspace(0, -3, 25)
+lambda1_range = np.logspace(0, -1, 30)
 modelselect_params = {'lambda1_range': lambda1_range}
 
-P.model_selection(modelselect_params = modelselect_params, method = 'eBIC', gamma = 1.0)
+P.model_selection(modelselect_params = modelselect_params, method = 'eBIC', gamma = 0.25)
 
 # regularization parameters are set to the best ones found during model selection
 print(P.reg_params)
