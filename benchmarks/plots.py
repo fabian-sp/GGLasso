12eb82ff1ec31e05b03db81c6393f48144a04df3
fabian-sp <fabian.schaipp@hotmail.de>
2021-06-28 18:42:18 +0200

    delete old bm data and change colors

--- benchmarks/plots.py
+++ benchmarks/plots.py
@@ -108,7 +108,9 @@ def plot_lambdas(df=pd.DataFrame(), upper_bound=float, lower_bound=float):
 
 
 def plot_bm(df=pd.DataFrame(), lambda_list=list, min_acc=1e-2, log_scale=True):
-
+    
+    col_dict = {"gglasso": '#264F73', "gglasso-block": "#3F7EA6", 'regain': '#F2811D', 'sklearn': '#C0C0C0'}
+    
     fig, axs = plt.subplots(len(lambda_list), 1, figsize=(6,10))
     j = 0
     for l1 in lambda_list:
@@ -116,7 +118,7 @@ def plot_bm(df=pd.DataFrame(), lambda_list=list, min_acc=1e-2, log_scale=True):
         df_sub = df[(df.l1 == l1) & (df.accuracy <= min_acc)]
         tmp = df_sub.groupby(["p", "N", "method"])["time"].min()
 
-        tmp.unstack().plot(ls='-', marker='o', xlabel="(p,N)", ylabel="runtime [sec]", ax=ax)
+        tmp.unstack().plot(ls='-', marker='o', xlabel="(p,N)", ylabel="runtime [sec]", ax=ax, color = col_dict)
         
         ax.set_title(rf"$\lambda_1$ = {l1}")
         ax.grid(linestyle='--')
