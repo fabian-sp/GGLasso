f9c1111411ab0556cd953ff9bed800119c28a7f5
fabian-sp <fabian.schaipp@hotmail.de>
2021-11-05 13:35:15 +0100

    v0.1.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ URL = 'https://github.com/fabian-sp/GGLasso'
 EMAIL = 'fabian.schaipp@tum.de'
 AUTHOR = 'Fabian Schaipp'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = "0.1.5"
+VERSION = "0.1.6"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
